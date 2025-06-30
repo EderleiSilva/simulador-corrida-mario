3cfe68988fbe69ba717f335785b37ac848a48249
Ederlei Silva <ederleipereiras1@hotmail.com>
2025-06-30 01:58:58 -0300

    Adicionado logica de vencendor da corrida

--- src/index.js
+++ src/index.js
@@ -77,12 +77,12 @@ async function playRaceEngine(character1, character2) {
             await logRollResult(character2.NOME, "poder", diceResult2, character2.PODER);
 
             if (powerResult1 > powerResult2 && character2.PONTOS > 0) {
-                console.log(`${character1.NOME} venceu o confronto! ${character2} perdeu 1 ponto 🐢`)
+                console.log(`${character1.NOME} venceu o confronto! ${character2.NOME} perdeu 1 ponto 🐢`)
                 character2.PONTOS--;
             }
 
             if (powerResult2 > powerResult1 && character1.PONTOS > 0) {
-                console.log(`${character2.NOME} venceu o confronto! ${character1} perdeu 1 ponto 🐢`)
+                console.log(`${character2.NOME} venceu o confronto! ${character1.NOME} perdeu 1 ponto 🐢`)
                 character1.PONTOS--;
             }
 
@@ -101,8 +101,23 @@ async function playRaceEngine(character1, character2) {
     }
 }
 
+async function declareWinner(character1, character2) {
+    console.log("Resultado Final: ");
+    console.log(`${character1.NOME}: ${character1.PONTOS} ponto(s)`);
+    console.log(`${character2.NOME}: ${character2.PONTOS} ponto(s)`);
+
+    if (character1.PONTOS > character2.PONTOS){
+        console.log(`\n${character1.NOME} venceu a corrida! Parabéns! 🏆`);
+    } else if (character2.PONTOS > character1.PONTOS){
+        console.log(`\n${character2.NOME} venceu a corrida! Parabéns! 🏆`);
+    } else {
+        console.log(`\nA corrida terminou em empate!`);
+    }
+}
+
 (async function main() {
     console.log(`🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando...\n`);
 
     await playRaceEngine(player1, player2);
+    await declareWinner(player1, player2);
 })();
